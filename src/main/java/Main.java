958167500b97f6bc202ac1cb7f78c43f366417f2
hoggspjot <Andreyid67@gmail.com>
2025-08-19 00:06:24 +0300

    init block

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        Foo foo = new Foo(1);
+        Foo foo2 = new Foo(2);
+        System.out.println(foo.getId());
+        System.out.println(foo2.getId());
     }
 }
+
+
+class Foo {
+    {
+        System.out.println("Нестатический блок инициализации");
+    }
+
+    private int id;
+
+    public Foo(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+
