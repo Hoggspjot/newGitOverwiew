f767efb1f239655707f73a1f321ef125e22fede7
Hoggspjot <vingisaar@gmail.com>
2025-09-09 22:47:59 +0300

    base repeat

---
+++ src/main/java/day090925/Main01.java
@@ -0,0 +1,14 @@
+package day090925;
+
+import java.util.Scanner;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
+        int minutes = (time / 60) % 60;
+        int hours = time / 3600 ;
+        int sec = time % 60;
+        System.out.printf("%d часов %d минут %d секунд",hours, minutes, sec);
+    }
+}
