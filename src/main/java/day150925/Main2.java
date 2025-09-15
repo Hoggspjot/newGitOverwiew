54728638b244a7bab304bf9982b6c2023a649928
Hoggspjot <vingisaar@gmail.com>
2025-09-15 23:15:44 +0300

    Math6

---
+++ src/main/java/day150925/Main2.java
@@ -0,0 +1,18 @@
+package day150925;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String[] strings = str.split(" ");
+        int min = Integer.parseInt(strings[0]);
+        for (int i = 1; i < strings.length; i++) {
+            if ( Integer.parseInt(strings[i]) < min) {
+                min = Integer.parseInt(strings[i]);
+            }
+        }
+        System.out.println(min);
+    }
+}
