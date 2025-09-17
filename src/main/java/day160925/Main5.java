ccf8c2afa5fd893340f056b022817675416d2a95
Hoggspjot <vingisaar@gmail.com>
2025-09-17 21:35:10 +0300

    simple

---
+++ src/main/java/day160925/Main5.java
@@ -0,0 +1,7 @@
+package day160925;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/day170925/Main.java
+++ src/main/java/day170925/Main.java
@@ -1,9 +1,36 @@
 package day170925;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String month = scanner.nextLine();
 
-
+        switch (month) {
+            case "Декабрь":
+            case "Январь":
+            case "Февраль":
+                System.out.println("Зима");
+                break;
+                case "март":
+                 case "Апрель":
+                 case "Май":
+                System.out.println("Весна");
+                break;
+                 case "Июнь":
+                 case "Июль":
+                 case "Август":
+                System.out.println("Лето");
+                break;
+                 case "Сентябрь":
+                 case "Октябрь":
+                 case "Ноябрь":
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
     }
 }
