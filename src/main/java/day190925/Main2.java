9c8c7964a03e1af313fa9ac7dd62f0ef9b8a909e
Hoggspjot <vingisaar@gmail.com>
2025-09-19 22:57:09 +0300

    simple5

--- src/main/java/day190925/Main2.java
+++ src/main/java/day190925/Main2.java
@@ -1,8 +1,19 @@
 package day190925;
 
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-
-
+        Scanner scanner = new Scanner(System.in);
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        int start = one < two ? one : two;
+        int finish = one > two ? one : two;
+        double number = 1;
+        while (start <= finish) {
+            number *= start;
+            start++;
+        }
+        System.out.println(number);
     }
 }
