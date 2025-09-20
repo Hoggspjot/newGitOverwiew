916ecb587dbf16174b8728687c8425d7f35525aa
Hoggspjot <vingisaar@gmail.com>
2025-09-20 22:29:28 +0300

    simple10

---
+++ src/main/java/day200925/Main4.java
@@ -0,0 +1,24 @@
+package day200925;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int type = scanner.nextInt();
+        int num = scanner.nextInt();
+        char lit = scanner.next().charAt(0);
+
+        if (type == 1 && num >= 0) {
+            for (int i = 0; i < num; i++) {
+                System.out.println(lit);
+            }
+        } else if (type == 2 && num >= 0) {
+            for (int i = 0; i < num; i++) {
+                System.out.print(lit);
+            }
+        } else {
+            System.out.println("ERROR");
+        }
+    }
+}
