8dae6cd55f2c4f86caac361ce57d00de24dd222b
Hoggspjot <vingisaar@gmail.com>
2025-09-22 21:40:06 +0300

    simple16

---
+++ src/main/java/day220925/Main4.java
@@ -0,0 +1,16 @@
+package day220925;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <=k ; j++) {
+                System.out.print(i * j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
