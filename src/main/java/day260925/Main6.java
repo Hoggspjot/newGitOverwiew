3cfb0be5fe0bc1f72b0cf4033418c69e2e12da49
Hoggspjot <vingisaar@gmail.com>
2025-09-27 00:11:12 +0300

    simple32

---
+++ src/main/java/day260925/Main6.java
@@ -0,0 +1,48 @@
+package day260925;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int[] array = new int[len];
+        Random random = new Random(n);
+
+        int max = Integer.MIN_VALUE;
+        int maxIdx = 0;
+        int min = Integer.MAX_VALUE;
+        int minIdx = 0;
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = random.nextInt(-5, 16);
+            System.out.print(array[i] + " ");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIdx = i;
+            }
+            if (array[i] < 0) {
+                min = array[i];
+                minIdx = i;
+                if (minIdx < i) {
+                    minIdx = i;
+                }
+            }
+        }
+
+        if (min < 0) {
+            int temp = array[minIdx];
+            array[minIdx] = array[maxIdx];
+            array[maxIdx] = temp;
+        }
+        System.out.println();
+        for (int x : array) {
+            System.out.print(x+" ");
+        }
+    }
+}
