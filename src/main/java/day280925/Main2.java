a79b8798b6452b8d712b71fb407390f1be53a10d
Hoggspjot <vingisaar@gmail.com>
2025-09-28 22:11:18 +0300

    simple37

---
+++ src/main/java/day280925/Main2.java
@@ -0,0 +1,33 @@
+package day280925;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int[][] array = new int[x][y];
+        Random random = new Random(scanner.nextInt());
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(-10, 11);
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < array[x - 1].length; i++) {
+            int sum = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i] > 0) {
+                    sum += array[j][i];
+                }
+            }
+            System.out.print(sum + " ");
+        }
+    }
+}
