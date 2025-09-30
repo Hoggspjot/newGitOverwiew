7a58b5653babda46d60d3b3c856e46ccb1a589fa
Hoggspjot <vingisaar@gmail.com>
2025-09-30 22:47:59 +0300

    simple43

---
+++ src/main/java/day300925/Main2.java
@@ -0,0 +1,14 @@
+package day300925;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        str = str.replaceAll(";", ".,");
+
+        System.out.println(str);
+    }
+}
