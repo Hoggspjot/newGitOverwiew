37eaf4c5d482d5fb3d5d07d8ee32e7264d5dab94
Hoggspjot <vingisaar@gmail.com>
2025-09-30 23:24:19 +0300

    simple44

---
+++ src/main/java/day300925/Main4.java
@@ -0,0 +1,26 @@
+package day300925;
+
+import java.util.Scanner;
+
+class Main4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        if (isGMailAddress(str)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static boolean isGMailAddress(String email) {
+        int first = email.indexOf("@");
+        int last = email.lastIndexOf("@");
+        String mailbox = email.substring(last+1);
+        if (first == last && mailbox.equals("gmail.com") && first > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
